0b242f46da596e1ac8969e1411ffd2a7e5dabdbf
alani.lyon <kianakilipaki@gmail.com>
2024-10-23 13:37:27 -0600

    add banner

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa'; // For the hamburger icon
 import './styles.scss';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="logo">
-      <Link to="/">BE READY</Link>
-    </div>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/blog">Blog</Link></li>
-      <li><Link to="/about">About</Link></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  return (
+    <>
+    <div className='banner'></div>
+    <nav className="navbar">
+      <ul className="navbar-menu">
+        <li className="active"><Link to="/">Home</Link></li>
+        <li><Link to="/presentations">Presentations</Link></li>
+        <li><Link to="/store">Store</Link></li>
+        <li><Link to="/links">Links</Link></li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/join-us">Join Us</Link></li>
+        <li><Link to="/forum">Forum</Link></li>
+      </ul>
+      <div className="navbar-hamburger">
+        <FaBars />
+      </div>
+    </nav>
+    </>
+  );
+};
 
 export default Navbar;
